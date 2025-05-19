d4601f4543ea7a8ef6eea046c8e499d0fc025020
Manuel G Rivera <12546268+mgrivera@users.noreply.github.com>
2025-05-19 08:42:30 -0400

    Deleting unnecessary comments and changing titles

--- Components/Pages/Tablas/Coberturas/Coberturas.razor.cs
+++ Components/Pages/Tablas/Coberturas/Coberturas.razor.cs
@@ -5,21 +5,21 @@ namespace scrweb_blazor.Components.Pages.Tablas.Coberturas
     public partial class Coberturas
     {
         // Detail's List 
-        private List<Cobertura_Form_Item>? _coberturas;
+        private List<Item_model>? _items;
 
         // Parent's List 
-        private List<Ramo>? _ramos; 
+        private List<Option>? _options; 
 
         // Just to use Alerts from MudBlazor 
         AlertSettings _alertSettings = new(Severity.Info, false, "");
 
         protected override async Task OnInitializedAsync()
         {
-            _coberturas = GetListItems();
-            _ramos = GetRamoItems();
+            _items = GetListItems();
+            _options = GetRamoItems();
 
             // para mostrar un mensaje al usuario 
-            string message = $"Ok, hemos leído <b>{10}</b> coberturas desde la base de datos.";
+            string message = $"Ok, we've read <b>{10}</b> items - They are shown in the list.";
             _alertSettings = new(Severity.Info, true, message);
         }
 
@@ -31,32 +31,32 @@ namespace scrweb_blazor.Components.Pages.Tablas.Coberturas
             StateHasChanged();
         }
 
-        private List<Cobertura_Form_Item> GetListItems()
+        private List<Item_model> GetListItems()
         {
-            var list = new List<Cobertura_Form_Item>() {
-                new Cobertura_Form_Item { Id = "number 1", Descripcion = "number 1", RamoId = "ramo 1" },
-                new Cobertura_Form_Item { Id = "number 2", Descripcion = "number 2", RamoId = "ramo 2" },
-                new Cobertura_Form_Item { Id = "number 3", Descripcion = "number 3", RamoId = "ramo 3" },
-                new Cobertura_Form_Item { Id = "number 4", Descripcion = "number 4", RamoId = "ramo 4" },
-                new Cobertura_Form_Item { Id = "number 5", Descripcion = "number 5", RamoId = "ramo 5" },
-                new Cobertura_Form_Item { Id = "number 6", Descripcion = "number 6", RamoId = "ramo 1" },
-                new Cobertura_Form_Item { Id = "number 7", Descripcion = "number 7", RamoId = "ramo 2" },
-                new Cobertura_Form_Item { Id = "number 8", Descripcion = "number 8", RamoId = "ramo 3" },
-                new Cobertura_Form_Item { Id = "number 9", Descripcion = "number 9", RamoId = "ramo 4" },
-                new Cobertura_Form_Item { Id = "number 10", Descripcion = "number 10", RamoId = "ramo 5" },
+            var list = new List<Item_model>() {
+                new Item_model { Id = "Id 1", Description = "number 1", OptionId = "Id 1" },
+                new Item_model { Id = "Id 2", Description = "number 2", OptionId = "Id 2" },
+                new Item_model { Id = "Id 3", Description = "number 3", OptionId = "Id 3" },
+                new Item_model { Id = "Id 4", Description = "number 4", OptionId = "Id 4" },
+                new Item_model { Id = "Id 5", Description = "number 5", OptionId = "Id 5" },
+                new Item_model { Id = "Id 6", Description = "number 6", OptionId = "Id 1" },
+                new Item_model { Id = "Id 7", Description = "number 7", OptionId = "Id 2" },
+                new Item_model { Id = "Id 8", Description = "number 8", OptionId = "Id 3" },
+                new Item_model { Id = "Id 9", Description = "number 9", OptionId = "Id 4" },
+                new Item_model { Id = "Id 10", Description = "number 10", OptionId = "Id 5" },
             };
 
             return list;
         }
 
-        private List<Ramo> GetRamoItems()
+        private List<Option> GetRamoItems()
         {
-            var list = new List<Ramo>() {
-                new Ramo ("ramo 1", "ramo 1"),
-                new Ramo ("ramo 2", "ramo 2"),
-                new Ramo ("ramo 3", "ramo 3"),
-                new Ramo ("ramo 4", "ramo 4"),
-                new Ramo ("ramo 5", "ramo 5"),
+            var list = new List<Option>() {
+                new Option ("Id 1", "Option 1"),
+                new Option ("Id 2", "Option 2"),
+                new Option ("Id 3", "Option 3"),
+                new Option ("Id 4", "Option 4"),
+                new Option ("Id 5", "Option 5"),
             };
 
             return list;
@@ -64,21 +64,21 @@ namespace scrweb_blazor.Components.Pages.Tablas.Coberturas
 
         // =====================================================================================================
         // this is the type for the List items 
-        public class Cobertura_Form_Item
+        public class Item_model
         {
             public string Id { get; set; } = string.Empty;
 
-            public string Descripcion { get; set; } = string.Empty;
+            public string Description { get; set; } = string.Empty;
 
-            public string? RamoId { get; set; } = string.Empty;
+            public string? OptionId { get; set; } = string.Empty;
         }
 
         // =====================================================================================================
         // this is the Type for the Select items
-        private record Ramo(string Id, string Descripcion);
+        private record Option(string Id, string Description);
 
         // ===============================================================================================================
         // para mostrar un mensaje al usuario con MudAlert 
-        private record AlertSettings(Severity Tipo, bool Show, string Text);
+        private record AlertSettings(Severity Type, bool Show, string Text);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,16 +1,8 @@
-//using Microsoft.AspNetCore.Components.Authorization;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.EntityFrameworkCore;
 using Microsoft.FluentUI.AspNetCore.Components;
 using scrweb_blazor.Components;
-//using scrweb_blazor.Components.Account;
-//using scrweb_blazor.Data;
-//using scrweb_blazor.Models.EF_Core;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-//builder.Services.AddControllers();
 
 // to add globalization to the app, we needed to add an MVC Controller 
 builder.Services.AddControllers();
@@ -21,40 +13,6 @@ builder.Services.AddFluentUIComponents();
 
 // Add MudBlazor components 
 builder.Services.AddMudServices();
-
-////builder.Services.AddCascadingAuthenticationState();
-//builder.Services.AddScoped<IdentityUserAccessor>();
-//builder.Services.AddScoped<IdentityRedirectManager>();
-//builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
-
-//builder.Services.AddAuthentication(options =>
-//    {
-//        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-//        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-//    })
-//    .AddIdentityCookies();
-
-var connectionString = builder.Configuration.GetConnectionString("scrweb_blazor") ?? throw new InvalidOperationException("Connection string 'scrweb_blazor' not found.");
-
-//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-//builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//    //.AddRoles<IdentityRole>()
-//    .AddEntityFrameworkStores<ApplicationDbContext>()
-//    //.AddSignInManager()
-//    //.AddDefaultTokenProviders()
-//    ;
-
-//builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//    //.AddEntityFrameworkStores<ApplicationDbContext>()
-//    ;
-
-//builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
-
-// Register factory and configure the options (and pass the connection string to the context) 
-//builder.Services.AddDbContextFactory<dbContext>(options => options.UseSqlServer(connectionString));
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -79,8 +37,5 @@ app.UseAntiforgery();
 app.MapControllers();
 
 app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-
-// Add additional endpoints required by the Identity /Account Razor components.
-//app.MapAdditionalIdentityEndpoints();
 
 app.Run();
